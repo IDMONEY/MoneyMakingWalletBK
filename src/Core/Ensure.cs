7fcf6b39276114208b874aaa1989dc659229352f
Nelson Badilla <nelbadiaz@outlook.com>
2018-09-10 22:00:19 -0600

    Adding some extension methods

---
+++ src/Core/Ensure.cs
@@ -0,0 +1,36 @@
+﻿#region Libraries
+using System;
+using System.Diagnostics;
+#endregion
+
+namespace IDMONEY.IO
+{
+    public static class Ensure
+    {
+        /// <summary>
+        /// Determines whether the given argument is not null.
+        /// </summary>
+        /// <param name="parameter">The value.</param>
+        [DebuggerStepThrough]
+        public static void IsNotNull(object parameter)
+        {
+            if (parameter.IsNull())
+            {
+                throw new ArgumentNullException(nameof(parameter));
+            }
+        }
+
+        /// <summary>
+        /// Determines whether the given <see cref="String"/> is null, empty or contains only white spaces.
+        /// </summary>
+        /// <param name="parameter">The string value.</param>
+        [DebuggerStepThrough]
+        public static void IsNotNullOrEmpty(string parameter)
+        {
+            if (String.IsNullOrWhiteSpace(parameter))
+            {
+                throw new ArgumentException($"{nameof(parameter)} cannot be null or empty");
+            }
+        }
+    }
+}
---
+++ src/Core/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,22 @@
+﻿#region Libraries
+using System;
+using System.Linq;
+using System.Security.Claims; 
+#endregion
+
+namespace IDMONEY.IO.Security
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static bool IsAuthenticated(this ClaimsPrincipal user)
+        {
+            return user?.Identity?.IsAuthenticated == true;
+        }
+
+        public static bool HasClaims(this ClaimsPrincipal user, params Predicate<Claim>[] requiredClaims)
+        {
+            return user.IsNotNull()
+                && requiredClaims.All(user.HasClaim);
+        }
+    }
+}
